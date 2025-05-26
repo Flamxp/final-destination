ff6c3404940bc98103c95b757441b0a20242c928
Flamxp <76263453+Flamxp@users.noreply.github.com>
2025-05-26 23:53:22 +0200

    a

--- logged in user/script/lightMode.js
+++ logged in user/script/lightMode.js
@@ -3,14 +3,28 @@ function switchMode() {
     const switch_icon = document.getElementById("switch-icon");
 
     if (body.classList.contains("switch-mode")) {
+        if (document.getElementById("change")) {
+            body.classList.remove("switch-mode");
+            switch_icon.classList.remove("fa-solid", "fa-moon");
+            switch_icon.classList.add("fa-solid", "fa-sun");
+            document.getElementById("change").style.color = "white";}
+        else {
         body.classList.remove("switch-mode");
-        document.getElementById("change").style.color = "white";
         switch_icon.classList.remove("fa-solid", "fa-moon");
         switch_icon.classList.add("fa-solid", "fa-sun");
+        }
+        
     } else {
+        if (document.getElementById("change")) {
+            body.classList.add("switch-mode");
+            switch_icon.classList.remove("fa-solid", "fa-sun");
+            switch_icon.classList.add("fa-solid", "fa-moon");
+            document.getElementById("change").style.color = "#0f172a";
+        }
+        else{
         body.classList.add("switch-mode");
-        document.getElementById("change").style.color = "#0f172a";
         switch_icon.classList.remove("fa-solid", "fa-sun");
         switch_icon.classList.add("fa-solid", "fa-moon");
+        }
     }
 }
